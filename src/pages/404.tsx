f20dd9f228b0af3255ef90ace2f72dcb0a40dab7
HyungjunJeon <maru105kr@gmail.com>
2022-02-08 22:28:02 +0900

    Implementing a 404 Error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,48 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+import GlobalStyle from 'components/Common/GlobalStyle'
+
+const NotFoundPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const NotFoundText = styled.p`
+  font-size: 150px;
+  font-weight: 800;
+`
+
+const NotFoundDescription = styled.p`
+  font-size: 25px;
+  text-align: center;
+  line-height: 1.3;
+`
+const GoToMainButton = styled(Link)`
+  margin-top: 30px;
+  font-size: 20px;
+  text-decoration: underline;
+
+  &: hover {
+    text-decoration: underline;
+  }
+`
+
+const NotFoundPage: FunctionComponent = function () {
+  return (
+    <NotFoundPageWrapper>
+      <GlobalStyle />
+      <NotFoundText>404</NotFoundText>
+      <NotFoundDescription>
+        찾을 수 없는 페이지입니다. <br />
+        다른 콘텐츠를 보러 가보시겠어요?
+      </NotFoundDescription>
+      <GoToMainButton to="/">메인으로</GoToMainButton>
+    </NotFoundPageWrapper>
+  )
+}
+
+export default NotFoundPage
