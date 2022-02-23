ac9fe9da672f4b584e50b2486a3cfb24563a7930
HyungjunJeon <maru105kr@gmail.com>
2022-02-23 23:38:11 +0900

    Fix commentwidget component error

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -2,7 +2,7 @@ import React, { createRef, FunctionComponent, useEffect } from 'react'
 import styled from '@emotion/styled'
 
 const src = 'https://utteranc.es/client.js'
-const repo = 'HyungjunJeon/HyungjunJeon-gitbub.io'
+const repo = 'HyungjunJeon/hyungjunjeon.github.io'
 
 type UtterancesAttributesType = {
   src: string
